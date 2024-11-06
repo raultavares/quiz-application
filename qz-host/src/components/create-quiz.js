20c82ae531d0bf836a50553c0cdf26e366fa31f0
Raul Tavares <raultavares@Rauls-MacBook-Air.local>
2024-11-06 10:29:50 +0000

    Load Quiz Demo Data

--- qz-host/src/components/create-quiz.js
+++ qz-host/src/components/create-quiz.js
@@ -25,6 +25,115 @@ const CreateQuiz = () => {
     }));
   };
 
+  const handleGenerateDemoQuiz = async (e) => {
+
+    setAlternatives([]);
+    setQuizID(null);
+    setQuizData({});
+
+    let quizObj = {
+      "quiz_id": "123456",
+      "qz_info": {
+       "qz_description": "Test your knowledge about the role of cats in ancient Egyptian culture and history.",
+       "qz_name": "Cats and Ancient Egypt Quiz"
+      },
+      "qz_questions": [
+       {
+        "qe_alternatives": [
+         "Bastet",
+         "Anubis",
+         "Ra",
+         "Horus"
+        ],
+        "qe_answer": 0,
+        "qe_picture": "",
+        "qe_question": "Which ancient Egyptian deity was represented as a cat or a cat-headed woman?"
+       },
+       {
+        "qe_alternatives": [
+         "Pets",
+         "Protectors of the home",
+         "Symbols of fertility",
+         "All of the above"
+        ],
+        "qe_answer": 3,
+        "qe_picture": "",
+        "qe_question": "What roles did cats serve in ancient Egyptian society?"
+       },
+       {
+        "qe_alternatives": [
+         "Mummified",
+         "Buried with their owners",
+         "Worshipped in temples",
+         "All of the above"
+        ],
+        "qe_answer": 3,
+        "qe_picture": "",
+        "qe_question": "How were cats honored and treated in ancient Egypt?"
+       },
+       {
+        "qe_alternatives": [
+         "Killing a cat was punishable by death",
+         "Cats were considered sacred animals",
+         "Cat mummies were used as offerings to the gods",
+         "All of the above"
+        ],
+        "qe_answer": 3,
+        "qe_picture": "",
+        "qe_question": "Which of the following statements about cats in ancient Egypt are true?"
+       },
+       {
+        "qe_alternatives": [
+         "Hunting rodents",
+         "Protecting grain stores",
+         "Catching birds",
+         "All of the above"
+        ],
+        "qe_answer": 3,
+        "qe_picture": "",
+        "qe_question": "What practical roles did cats play in ancient Egyptian households?"
+       },
+       {
+        "qe_alternatives": [
+         "Bastet",
+         "Sekhmet",
+         "Isis",
+         "Nephthys"
+        ],
+        "qe_answer": 1,
+        "qe_picture": "",
+        "qe_question": "Which ancient Egyptian goddess was depicted as a lioness or a woman with a lion's head?"
+       },
+       {
+        "qe_alternatives": [
+         "Bubastis",
+         "Thebes",
+         "Memphis",
+         "Giza"
+        ],
+        "qe_answer": 0,
+        "qe_picture": "",
+        "qe_question": "Which ancient Egyptian city was known for its cat cult and had a temple dedicated to the cat goddess Bastet?"
+       },
+       {
+        "qe_alternatives": [
+         "Hunting",
+         "Fertility",
+         "Protection",
+         "All of the above"
+        ],
+        "qe_answer": 3,
+        "qe_picture": "",
+        "qe_question": "Which of the following qualities were associated with cats in ancient Egyptian mythology and symbolism?"
+       }
+      ]
+     }
+
+    
+    setQuizData(quizObj);
+    setAlternatives(quizObj.qz_questions)
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setAlternatives([])
@@ -154,6 +263,7 @@ const CreateQuiz = () => {
           >
             {isLoading ? 'Generating Quiz! Please wait...' : 'Generate Quiz'}
           </Button>
+          <Button variant='contained' className='gen-demo-quiz-button' onClick={handleGenerateDemoQuiz}>Load Demo Quiz</Button>
         </form>
 
       </div>
